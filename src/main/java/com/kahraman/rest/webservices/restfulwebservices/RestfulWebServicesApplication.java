73f1b4071aedb859936766e10290657099045c0e
Abdullah Kahraman <a.kahramnn@gmail.com>
2024-01-24 01:52:05 +0300

    first-commit

---
+++ src/main/java/com/kahraman/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,15 @@
+package com.kahraman.rest.webservices.restfulwebservices;
+
+import jakarta.persistence.Entity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
